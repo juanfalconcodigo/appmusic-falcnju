4f75d2ed69a31bcf51942d21affd464753fb292f
JUANFALCONCODIGO <JUANCANEN_10_@HOTMAIL.COM>
2023-04-24 11:48:37 -0500

    [ADD] Added ngx-translate in the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appmusic-falcnju';
+  constructor(public translate: TranslateService) {
+    let me = this;
+    me.translate.addLangs(['en', 'es']);
+    const lang = me.translate.getBrowserLang();
+    if ((lang !== 'es') && (lang !== 'en')) {
+      me.translate.setDefaultLang('en');
+    }else{
+      console.log('[BROWSERLANG]',lang);
+      me.translate.setDefaultLang(lang);
+    }
+  }
+
+  switchLang(lang: string) {
+    let me = this;
+    me.translate.use(lang);
+  }
 }
